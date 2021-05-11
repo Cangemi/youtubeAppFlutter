c57c9a06af5d2440b75a856b11df998f79b9283e
Cangemi <65517004+Cangemi@users.noreply.github.com>
2021-05-11 17:45:36 -0300

    api youtube

---
+++ lib/Api.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
+const CHAVE_YOUTUBE_API = "AIzaSyC08MJFACOK5t1tg8Dtza1yuRG98cKZJ38";
+const ID_CANAL = "UCc4K7bAqpdBP8jh1j9XZAww";
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+class Api {
+
+  pesquisar (String pesquisa) async{
+
+    http.Response response = await http.get(
+      URL_BASE + "search"
+          "?part=snippet"
+          "&type=video"
+          "&maxResults=20"
+          "&order=date"
+          "&key=$CHAVE_YOUTUBE_API"
+          "&chanelId="
+          "&q=$pesquisa"
+    );
+
+    if(response.statusCode == 200){
+      print("Sucesso");
+      Map<String, dynamic> dadosJson = json.decode(response.body);
+    }
+
+  }
+
+}
